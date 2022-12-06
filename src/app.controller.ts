5fb20c43e245ab9c7a4ddab1b0208d4ad2aedea5
diegofvm20 <66849766+diegofvm20@users.noreply.github.com>
2022-12-06 02:10:00 -0500

    update app.ts
    Se completo la tarea

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,74 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import { threadId } from 'worker_threads';
 import { AppService } from './app.service';
+
+interface Carro {
+  marca: string,
+  color: string,
+  modelo: number,
+  placa: string
+
+  
+}
 
 @Controller()
 export class AppController {
-   constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-   private persona = "Mundo";
+  private Carros : Carro[] = [{
+    marca: "mazda",
+    color: "rojo",
+    modelo: 2007,
+    placa: "shj123"
+  }]
 
-   @Get()
-   getHello(): string {
-      return `Hola: ${this.persona}`
-   }
+  @Get(':nombre/:apellido/:edad')
+  findWithSize( @Param() params) {
+    return `En esta ruta obtenemos el producto ${params.id}, pero en su tamaño ${params.size}`;
+  }
 
-   @Post(':nombre')
-   modificar(@Param('nombre') nombre: string): string {
-      this.persona = nombre;
-      return `Mensaje modificado: ${this.persona}`
-   }
+  @Get()
+  getHello(): Carro[] {
+    return this.Carros;
+  }
+
+
+  @Post()
+  crear(@Body() datos: Carro): Carro {
+    this.Carros.push(datos);
+    return datos;
+  }
+
+  @Put(":id")
+  modificar(@Body() datos: Carro, @Param('id') id: number): Carro | string {
+    try{
+    this.Carros[id] = datos
+    return this.Carros[id];
+    }
+    catch{
+      return `No fue posible modificar al usuario en la posición ${id}`
+    }
+  }
+
+  @Delete(":id")
+  eliminar(@Param('id') id: number){
+    try{
+      this.Carros = this.Carros.filter((val, index) => index != id);
+      return true;
+    }
+    catch{
+      return false;
+    }
+  }
+
+  @Patch(":id/modelo/:modelo")
+  cambiarEdad(@Param('modelo') id: number, @Param('modelo') modelo: number): Carro | string{
+    try{
+      this.Carros[id].modelo = modelo;
+      return this.Carros[id];
+    }
+    catch{
+      return `No fue posible modificar al usuario en la posición ${id}`
+    }
+  }
 }
-
