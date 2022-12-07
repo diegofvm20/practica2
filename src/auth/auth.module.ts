9f8702c24216c5915c6ebfa6dd923e494db02890
diegofvm20 <diegofvm@unicauca.edu.co>
2022-12-06 22:49:35 -0500

    Se completo lapractica 3

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LocalStrategy } from './local.strategy';
 import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
-import { LocalStrategy } from './local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { JwtStrategy } from './jwt-auth.strategy';
+import { jwtSecret } from 'src/constant';
+
 
 @Module({
-   imports: [UsersModule, PassportModule],
-   providers: [AuthService, LocalStrategy]
-})
+   controllers: [AuthController],
+   imports: [
+      UsersModule,
+      PassportModule,
+      JwtModule.register({
+         secret: "este es el secreto para generar JWT",
+         signOptions: { expiresIn: '60m' },
+      }),
+   ],
+   providers: [AuthService, LocalStrategy],
+   exports: [AuthService],
+   })
 export class AuthModule {}
